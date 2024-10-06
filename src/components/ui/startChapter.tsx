8a8e5e544731ccd884470e57364dbd48f7bcccc9
AlexandreOkita <xande.okita@gmail.com>
2024-10-06 11:42:24 -0300

    add fadein on start chapter

--- src/components/ui/startChapter.tsx
+++ src/components/ui/startChapter.tsx
@@ -1,9 +1,22 @@
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
 type Props = {
   chapterNumber: string;
   chapterTitle: string;
 };
 
 export default function StartChapter({ chapterNumber, chapterTitle }: Props) {
+  const [showContinue, setShowContinue] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContinue(true)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+  
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <img
@@ -12,12 +25,29 @@ export default function StartChapter({ chapterNumber, chapterTitle }: Props) {
         className="absolute inset-0 w-full h-full object-cover"
         style={{ filter: "brightness(0.4)" }}
       />
-      <div className="absolute inset-0 flex items-center justify-center text-[#ECECEC]">
-        <div className="text-center">
-          <h1 className="text-7xl tracking-widest">{chapterNumber}</h1>
-          <br></br>
-          <h1 className="text-6xl tracking-widest">{chapterTitle}</h1>
-        </div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-[#ECECEC] space-y-8">
+        <motion.div
+          className="text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+        >
+          <div className="text-center">
+            <h1 className="text-7xl tracking-widest">{chapterNumber}</h1>
+            <br></br>
+            <h1 className="text-6xl tracking-widest">{chapterTitle}</h1>
+          </div>
+        </motion.div>
+        {showContinue && <motion.div
+          className="text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 4.5, ease: "easeInOut" }}
+        >
+          <div className="text-center">
+            <h1 className="text-2xl tracking-widest">Click to Continue</h1>
+          </div>
+        </motion.div>}
       </div>
     </div>
   );
